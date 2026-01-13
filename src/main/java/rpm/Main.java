b790696758725257e181ec162d958fc2c92a1a78
Ruitong Li <rl2523@ic.ac.uk>
2026-01-13 17:05:14 +0000

    Change the patient IDs back  (P1-P8)

--- src/main/java/rpm/Main.java
+++ src/main/java/rpm/Main.java
@@ -49,14 +49,14 @@ public class Main {
         AlertEngine alertEngine = new AlertEngine();
 
         List<Patient> patients = List.of(
-                new Patient("P001", "Patient 1", 34, "Ward A", "p1@hospital.com", "EC1"),
-                new Patient("P002", "Patient 2", 56, "Ward A", "p2@hospital.com", "EC2"),
-                new Patient("P003", "Patient 3", 41, "Ward B", "p3@hospital.com", "EC3"),
-                new Patient("P004", "Patient 4", 28, "Ward B", "p4@hospital.com", "EC4"),
-                new Patient("P005", "Patient 5", 73, "Ward C", "p5@hospital.com", "EC5"),
-                new Patient("P006", "Patient 6", 62, "Ward C", "p6@hospital.com", "EC6"),
-                new Patient("P007", "Patient 7", 39, "Ward D", "p7@hospital.com", "EC7"),
-                new Patient("P008", "Patient 8", 45, "Ward D", "p8@hospital.com", "EC8")
+                new Patient("P1", "Patient 1", 34, "Ward A", "p1@hospital.com", "EC1"),
+                new Patient("P2", "Patient 2", 56, "Ward A", "p2@hospital.com", "EC2"),
+                new Patient("P3", "Patient 3", 41, "Ward B", "p3@hospital.com", "EC3"),
+                new Patient("P4", "Patient 4", 28, "Ward B", "p4@hospital.com", "EC4"),
+                new Patient("P5", "Patient 5", 73, "Ward C", "p5@hospital.com", "EC5"),
+                new Patient("P6", "Patient 6", 62, "Ward C", "p6@hospital.com", "EC6"),
+                new Patient("P7", "Patient 7", 39, "Ward D", "p7@hospital.com", "EC7"),
+                new Patient("P8", "Patient 8", 45, "Ward D", "p8@hospital.com", "EC8")
         );
 
         PatientManager pm = new PatientManager(patients, maxSeconds, sampleHz, alertEngine);
